94af64c2db4a8a20571fbb6b2af859ea34b6ed59
Sameer kumar <2006Sameer2009@gmail.com>
2024-08-29 22:44:16 +0530

    create file upload component with file validation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
+import { VStack } from "@chakra-ui/react";
+import FileUpload from "./../components/HomePage/FileUpload";
+
 const HomePage = () => {
     return (
         <>
-            <div>Hello from home page of route path "/"</div>
+            <VStack spacing={8} p={8} w="full">
+                <FileUpload />
+                <div>Hello from home page of route path "/"</div>
+            </VStack>
         </>
     );
 };
