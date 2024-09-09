3f9de3eb297f9bac058b48315ae112f8faad4455
Sameer kumar <2006Sameer2009@gmail.com>
2024-09-09 19:39:43 +0530

    add drawer design navbar in moblie view

--- src/components/HomeLayout/navbar/mobile-nav/index.tsx
+++ src/components/HomeLayout/navbar/mobile-nav/index.tsx
@@ -1,20 +1,56 @@
-import { Collapse, List } from "@chakra-ui/react";
+import {
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Heading,
+    Link as ChakraLink,
+    List,
+} from "@chakra-ui/react";
 import { MenuProps } from "..";
 import { MobileNavItem } from "./mobile-nav-item";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 type MobileNavProps = {
     isOpen: boolean;
     items: MenuProps;
+    onClose: () => void;
 };
 
-export function MobileNav({ items, isOpen }: MobileNavProps) {
+export function MobileNav({ items, isOpen, onClose }: MobileNavProps) {
     return (
-        <Collapse in={isOpen} animateOpacity style={{ width: "100%" }}>
-            <List gap={2} spacing={2} width="100%" borderWidth={1} padding={2} bg="gray.100">
-                {items.map((item) => (
-                    <MobileNavItem key={item.label} {...item} />
-                ))}
-            </List>
-        </Collapse>
+        <>
+            <Drawer placement={"left"} size={"xs"} onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader borderBottomWidth="1px">
+                        <ChakraLink as={ReactRouterLink} to="/" _hover={{ textDecoration: "none" }}>
+                            <Heading
+                                as="h3"
+                                size="lg"
+                                color="teal"
+                                fontWeight="black"
+                                cursor="pointer"
+                            >
+                                Documenso
+                            </Heading>
+                        </ChakraLink>
+                    </DrawerHeader>
+                    <DrawerBody px={2}>
+                        <List gap={2} spacing={2} width="100%" fontSize={"large"}>
+                            {items.map((item) => (
+                                <MobileNavItem key={item.label} {...item} />
+                            ))}
+                        </List>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </>
+        // <Collapse in={isOpen} animateOpacity style={{ width: "100%" }}>
+
+        // </Collapse>
     );
 }
